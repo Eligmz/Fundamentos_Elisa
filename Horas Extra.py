9eb0dc0f02eb1bb44e70c3e9ce2d2094e12ac9c3
Eligmz <gomezmendezelisaa@gmail.com>
2024-10-03 20:33:07 -0600

    Create Horas Extra.py

---
+++ Horas Extra.py
@@ -0,0 +1,21 @@
+Realizar el programa de horas extra
+
+Hors = int (input("Numero de horas que trabajadas")
+
+Tarifa_Horstrab = 200
+Tarifa_Horsextra = 350
+
+if: Horstrab > 8: 
+horsextra = horstrab - 8
+pagonormal = horstrab * Tarifa_Horstrab
+pagonormal = 8 * 200
+pagoextra = Horsextra * Tarifa_Horsextra
+pagoextra = Horsextra * 350
+pago_tot = pagoextra + pagonormal 
+print ("pagotot")
+
+else: pagotot
+pagonormal = horstrab * Tarifa_Horstrab
+pagonormal = 8 * 200
+pagonormal = pagonormal
+print ("pagotot")
