14c2c7db6b0b67c50b639ffb4d09476aa9efe6af
Eligmz <gomezmendezelisaa@gmail.com>
2024-10-03 19:41:10 -0600

    Create Calificaciones.py

---
+++ Calificaciones.py
@@ -0,0 +1,9 @@
+# Realiza un programa que pida 3 calificaciones y calcule el promedio 
+
+Calificacion1 = int (input ("NUMERO DE LA PRIMERA CALIFICACION"))
+Calificacion2 = int (input ("NUMERO DE LA SEGUNDA CALIFICACION"))
+Calificacion3 = int (input ("NUMERO DE LA TERCERA CALIFICACION3"))
+
+Prom = (Calificacion1 + Calificacion2 + Calificacion3) / 3 
+
+print ("El promedio de las calificaciones es:", Prom) 
